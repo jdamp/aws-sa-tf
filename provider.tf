154e854212f2bfb96178ca1772eb82cd72275941
Johannes Damp <jdamp@users.noreply.github.com>
2024-01-06 15:24:41 +0100

    Add profile setting for aws provider

--- provider.tf
+++ provider.tf
@@ -6,3 +6,9 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  region                  = "us-east-1"
+  shared_credentials_files = ["/home/jdamp/.aws/credentials"]
+  profile                 = "iamadmin-prod"
+}
