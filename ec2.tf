a350ede7d7f766ee267a2c28a53e17833d65c370
Johannes Damp <jdamp@users.noreply.github.com>
2024-01-10 21:54:26 +0100

    Working version of ec2 instance

--- ec2.tf
+++ ec2.tf
@@ -12,21 +12,21 @@ data "aws_ami" "amazon_linux" {
 }
 
 resource "aws_security_group" "allow_ssh" {
-  name = "MyFirstInstanceSG"
+  name        = "MyFirstInstanceSG"
   description = "Allow SSH inbound traffic"
-  
+
   ingress {
     description = "SSH from anywhere"
-    from_port = 22
-    to_port = 22
-    protocol = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
-    egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -34,11 +34,16 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
+resource "aws_key_pair" "a4l" {
+  key_name   = "A4L"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
 
 resource "aws_instance" "myfirstec2" {
-  ami           = data.aws_ami.amazon_linux
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.a4l_key.key_name
+  key_name      = aws_key_pair.a4l.key_name
 
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
@@ -50,4 +55,13 @@ resource "aws_instance" "myfirstec2" {
   tags = {
     Name = "MyFirstEC2Instance"
   }
+}
+
+resource "aws_ec2_instance_state" "myfirstec2" {
+  instance_id = aws_instance.myfirstec2.id
+  state       = "stopped"
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.myfirstec2.public_ip
 }
--- imports.tf
+++
@@ -1,5 +0,0 @@
-
-import {
-  to = aws_key_pair.bla
-  id = "key-07617ba236735873e"
-}
